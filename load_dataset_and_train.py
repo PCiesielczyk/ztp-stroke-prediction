af79262eaedefd0cf50d6ece51678c44237abb72
PCiesielczyk <piotrciesielczyk2001@gmail.com>
2024-06-08 19:27:13 +0200

    feat(streams): process datasets as streams with periodically poll

--- load_dataset_and_train.py
+++ load_dataset_and_train.py
@@ -4,18 +4,43 @@ from pyspark.ml import Pipeline
 from pyspark.ml.classification import LogisticRegression
 from pyspark.ml.feature import VectorAssembler
 from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType
 
 from metrics import print_metrics
 from preprocessing import encode_dataframe
+
+POLL_INTERVAL = "60 seconds"
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(levelname)s %(name)s %(message)s',
                     datefmt='%m/%d/%Y %I:%M:%S')
 logger = logging.getLogger('load_dataset_and_train')
 
-if __name__ == '__main__':
-    dataset_path = "dataset.csv"
-    spark = SparkSession.builder.master("local").appName("WordCount").getOrCreate()
+dataset_schema = StructType([
+    StructField("id", StringType(), True),
+    StructField("gender", StringType(), True),
+    StructField("age", StringType(), True),
+    StructField("hypertension", StringType(), True),
+    StructField("heart_disease", StringType(), True),
+    StructField("ever_married", StringType(), True),
+    StructField("work_type", StringType(), True),
+    StructField("Residence_type", StringType(), True),
+    StructField("avg_glucose_level", StringType(), True),
+    StructField("bmi", StringType(), True),
+    StructField("smoking_status", StringType(), True),
+    StructField("stroke", StringType(), True)
+])
+
+spark = SparkSession.builder.master("local").appName("WordCount").getOrCreate()
+
+
+def process_stream(data_frame, epoch_id):
+    dataset_path = "datasets_temp"
+    data_frame.write \
+        .format("csv") \
+        .mode("overwrite") \
+        .save(dataset_path)
+
     df = spark.read.option("header", True).csv(dataset_path)
 
     logger.info(f"Loaded {dataset_path}. Row count: {df.count()}, {len(df.columns)} columns in the dataset.")
@@ -48,4 +73,15 @@ if __name__ == '__main__':
     lr_predictions = model.transform(test_data)
     print_metrics(model, lr_predictions)
 
+
+if __name__ == '__main__':
+    streaming_df = spark.readStream \
+        .schema(dataset_schema) \
+        .csv("datasets")
+
+    query = streaming_df.writeStream \
+        .foreachBatch(process_stream) \
+        .trigger(processingTime=POLL_INTERVAL) \
+        .start()
+    query.awaitTermination()
     spark.stop()
