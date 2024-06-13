df8d61d09f0a23106a6610cc89c7d46561f6ef25
filbary <panbaryczka123@gmail.com>
2024-06-13 22:32:19 +0200

    add docker image build

--- run.bat
+++ run.bat
@@ -1,1 +1,2 @@
+docker build -f Dockerfile.spark -t myspark .
 docker exec -it spark-master bin/spark-submit --master spark://spark-master:7077 /data/spark-scripts/load_dataset_and_train.py
--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+set -e
 
+# build custom docker spark image
+sudo docker build -f Dockerfile.spark -t myspark .
+
+chmod +777 datasets_temp metrics model
+
+# submit script to master
 sudo docker exec -it spark-master bin/spark-submit --master spark://spark-master:7077 /data/spark-scripts/load_dataset_and_train.py
