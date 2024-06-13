d9801126812a168f4c6cd2cbb7fc81cb218f5421
filbary <panbaryczka123@gmail.com>
2024-06-13 22:37:33 +0200

    add docker-compose to scripts

--- run.bat
+++ run.bat
@@ -1,2 +1,3 @@
 docker build -f Dockerfile.spark -t myspark .
+sudo docker-compose up --scale spark-worker=2
 docker exec -it spark-master bin/spark-submit --master spark://spark-master:7077 /data/spark-scripts/load_dataset_and_train.py
--- run.sh
+++ run.sh
@@ -6,5 +6,7 @@ sudo docker build -f Dockerfile.spark -t myspark .
 
 chmod +777 datasets_temp metrics model
 
+sudo docker-compose up --scale spark-worker=2
+
 # submit script to master
 sudo docker exec -it spark-master bin/spark-submit --master spark://spark-master:7077 /data/spark-scripts/load_dataset_and_train.py
