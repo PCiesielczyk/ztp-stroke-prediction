1eada4a134ed9b63a9944f331d07e62d7c40c7c3
PCiesielczyk <piotrciesielczyk2001@gmail.com>
2024-06-13 22:33:17 +0200

    feat(metrics): saving metrics to text file

--- spark-scripts/metrics.py
+++ spark-scripts/metrics.py
@@ -12,22 +12,28 @@ logging.basicConfig(level=logging.INFO,
 logger = logging.getLogger('metrics')
 
 label = "label"
+current_directory = os.getcwd()
+metrics_path = 'metrics'
+metrics_txt = 'metrics.txt'
+metrics_relative = os.path.join(current_directory, metrics_path, metrics_txt)
 
 
 def create_directory(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
 
+create_directory(metrics_path)
 
 def print_and_plot_roc_curve(training_summary, predictions):
     evaluator = BinaryClassificationEvaluator(labelCol=label)
     area_under_curve = evaluator.evaluate(predictions)
 
+    with open(metrics_relative, 'a') as file:
+        file.write(f"Area under ROC curve: {area_under_curve}\n")
+
     logger.info(f"Area under ROC curve: {area_under_curve}")
 
     lrROC = training_summary.roc.toPandas()
-
-    create_directory('/data/metrics')
 
     plt.figure(figsize=(8, 6))
     plt.plot(lrROC['FPR'], lrROC['TPR'])
@@ -52,6 +58,10 @@ def print_accuracy_precision_recall_f1(training_summary):
     precision = training_summary.weightedPrecision
     recall = training_summary.weightedRecall
 
+    with open(metrics_relative, 'a') as file:
+        file.write("Accuracy: %s\nFPR: %s\nTPR: %s\nF-measure: %s\nPrecision: %s\nRecall: %s\n"
+                % (accuracy, falsePositiveRate, truePositiveRate, fMeasure, precision, recall))
+
     logger.info("Accuracy: %s\nFPR: %s\nTPR: %s\nF-measure: %s\nPrecision: %s\nRecall: %s"
                 % (accuracy, falsePositiveRate, truePositiveRate, fMeasure, precision, recall))
 
@@ -59,6 +69,10 @@ def print_accuracy_precision_recall_f1(training_summary):
 def print_coefficients_intercept(model):
     logger.info("Coefficients: \n" + str(model.coefficientMatrix))
     logger.info("Intercept: " + str(model.interceptVector))
+
+    with open(metrics_relative, 'a') as file:
+        file.write("Coefficients: \n" + str(model.coefficientMatrix) + '\n'
+                   "Intercept: " + str(model.interceptVector) + '\n')
 
 
 def print_loss(predictions):
@@ -73,6 +87,9 @@ def print_loss(predictions):
     )
     average_log_loss = log_loss.agg({"log_loss": "mean"}).collect()[0]["avg(log_loss)"]
     logger.info(f"Average loss: {average_log_loss}")
+
+    with open(metrics_relative, 'a') as file:
+        file.write(f"Average loss: {average_log_loss}")
 
 
 def print_metrics(model, predictions):
